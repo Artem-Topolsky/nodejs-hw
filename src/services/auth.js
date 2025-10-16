c515ba7b55701866e18eed36470b4542200d61b0
Artem Topolsky <almost210690@gmail.com>
2025-10-16 16:54:25 +0300

    fix secure: true in auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -17,21 +17,21 @@ export const setSessionCookies = (res, session) => {
 
   res.cookie('accessToken', session.accessToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: 'none',
     maxAge: FIFTEEN_MINUTES,
   });
 
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: 'none',
     maxAge: ONE_DAY,
   });
 
   res.cookie('sessionId', session._id.toString(), {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: 'none',
     maxAge: ONE_DAY,
   });
